8027c631e332c1b6f2f78f840d6e6bae44886f8a
Doriana <doriana.scaffo94@gmail.com>
2024-04-05 21:20:17 +0200

    I-added-the-second-exercise-html

---
+++ lw_04/exercises/exercise-2.js
@@ -0,0 +1,25 @@
+const myRoutine = [
+  "Mi alzo dal letto",
+  "Mi lavo e mi vesto",
+  "Preparo un caffè",
+  "Vado a lavoro",
+  "Faccio colazione",
+  "Inizio a lavorare",
+  "Torno a casa",
+  "Pranzo",
+  "Studio",
+  "Ceno",
+  "Dormo",
+];
+
+for (let index = 0; index < myRoutine.length; index++) {
+  console.log(myRoutine);
+}
+
+// for (index in myRoutine) {
+//   console.log(index + ") " + myRoutine[index]);
+// }
+
+// for (index of myRoutine) {
+//   console.log(index);
+// }
