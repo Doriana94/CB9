c9ff92cb539af03d5186efffbd1fc1a2c88ac897
Doriana <doriana.scaffo94@gmail.com>
2024-04-13 18:13:14 +0200

    Added exe-1.html

---
+++ lw_05/exercises/js/exe-1.js
@@ -0,0 +1,21 @@
+function countVocal(name) {
+  let count = 0;
+  for (let i = 0; i < name.length; i++) {
+   if (name[i] === "a" || name[i] === "e" || name[i] === "i" || name[i] === "o" || name[i] === "u") 
+  {
+    
+     count++;
+}
+   }
+   return count;
+} 
+ const vocals = ["a", "e", "i", "o", "u"];
+
+ const welcomeUtent = prompt("Benvenuto in questa nuova avventura!");
+
+ const name = "Doriana";
+
+  const numbersVocal = countVocal(welcomeUtent);
+
+ console.log("Il numero totale di vocali nella stringa è:", numbersVocal);
+
