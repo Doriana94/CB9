3d15a6bdc9b4a12c186e1caf5b44d5840e45a6ba
Doriana <doriana.scaffo94@gmail.com>
2024-04-15 11:26:43 +0200

    Added exe_advanced-1.html

---
+++ lw_05/exercises/js/exe_advanced-1.js
@@ -0,0 +1,25 @@
+function incrementedSalary(person) {
+  const peopleToIncrease = person
+    .filter((person) => person.pay < 3000)
+    .map((person) => {
+       return person.name + ": " +  person.pay * 1.1
+    });
+  return peopleToIncrease;
+}
+
+const person = [
+  {
+    name: "Mario",
+    pay: 2500,
+  },
+
+  { name: "Anna", pay: 3200 },
+
+  { name: "Luigi", pay: 2800 },
+
+  { name: "Laura", pay: 2900 }
+];
+
+const newPerson = incrementedSalary(person);
+
+console.log(newPerson);
