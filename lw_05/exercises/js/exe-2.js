f553bdb250c0667b1f3a1c53f3f0f08091cdb613
Doriana <doriana.scaffo94@gmail.com>
2024-04-16 12:31:05 +0200

    Modified exe-2.js

--- lw_05/exercises/js/exe-2.js
+++ lw_05/exercises/js/exe-2.js
@@ -1,13 +1,14 @@
-function highValue(value) {
+function highValue(numbers) {
   //Ho creato una costante per trovare il valore massimo dell'array//
   const filterValue = Math.max(...numbers);
 
   //Ho filtrato l'array inserendo solo il valore massimo//
-  const result = numbers.filter((number) => number === filterValue);
+  const result = numbers.filter((num) => num === filterValue);
 
   //Ho restituito l'array filtrato con il valore massimo//
   return result;
 }
 
 const numbers = [5, 17, 11, 22, 23];
+
 console.log("Il valore più alto dell'array è:", highValue(numbers));
