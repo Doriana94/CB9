a56217045341f4ac6dae3af3f60c119438f48721
Doriana <doriana.scaffo94@gmail.com>
2024-04-18 10:22:18 +0200

    Added file exe-1.html

---
+++ lw_06/exercises/js/exe-1.js
@@ -0,0 +1,13 @@
+function calculateAverage(arrNumbers){
+let sum = 0;
+for (let i = 0; i < arrNumbers.length; i++) {
+   sum += arrNumbers[i];   
+}
+return sum / arrNumbers.length;
+}
+
+
+const arrNumbers = [5, 12, 3, 8, 20, 17, 9];
+const average = calculateAverage(arrNumbers);
+
+console.log(average);
