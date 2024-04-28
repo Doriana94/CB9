df5f762730ea0842f865cddb695440c4ac5decf1
Doriana <doriana.scaffo94@gmail.com>
2024-04-28 13:32:10 +0200

    changed exe-2.js lw_07

--- lw_07/exercises/js/exe-2.js
+++ lw_07/exercises/js/exe-2.js
@@ -1,65 +1,52 @@
 const genImageCardEl = (imageData) => {
-const imageEl = document.createElement('img');
+  const imageEl = document.createElement("img");
 
-imageEl.className = 'card--image';
-imageEl.src = imageData.urls.small;
-imageEl.alt =imageData.alt_description;
+  imageEl.className = "card--image";
+  imageEl.src = imageData.urls.small;
+  imageEl.alt = imageData.alt_description;
 
-imageEl.addEventListener('click', () => {
+  imageEl.addEventListener("click", () => {
     window.open(imageData.urls.full, "_blank").focus();
-});
+  });
 
-return imageEl;
-}
+  return imageEl;
+};
 
 const genImageListEl = () => {
-const wrapperEl = document.createElement('div');
+  const wrapperEl = document.createElement("div");
 
+  wrapperEl.className = "card--list";
 
-wrapperEl.className = 'card--list';
+  return wrapperEl;
+};
 
+const BASE_URL = "https://api.unsplash.com";
+const API_TOKEN = "wDXQAagn2wnXHkg7A_3nqwSIYDJ9SicxLKLnisDp1B0";
+const SEARCH_QUERY = "cat";
+const rootEl = document.querySelector("#root");
 
-return wrapperEl;
-}
+const wrapperEl = document.createElement("form");
+const inputEl = document.createElement("input");
+const buttonEl = document.createElement("button");
 
-const BASE_URL = 'https://api.unsplash.com';
-const API_TOKEN = 'wDXQAagn2wnXHkg7A_3nqwSIYDJ9SicxLKLnisDp1B0';
-const SEARCH_QUERY = 'cat';
-const rootEl = document.querySelector('#root');
-
-const wrapperEl = document.createElement('form');
-const inputEl = document.createElement('input');
-const buttonEl = document.createElement('button');
-
-wrapperEl.className = 'input--elements';
-inputEl.placeholder = 'Cerca immagine';
-buttonEl.textContent = 'Cerca';
+wrapperEl.className = "input--elements";
+inputEl.placeholder = "Cerca immagine";
+buttonEl.textContent = "Cerca";
 
 wrapperEl.append(inputEl, buttonEl);
-rootEl.append(wrapperEl );
-
-
-
+rootEl.append(wrapperEl);
 
 fetch(`${BASE_URL}/search/photos/?client_id=${API_TOKEN}&query=${SEARCH_QUERY}`)
- .then((res) => res.json())
- .then((data) => {
+  .then((res) => res.json())
+  .then((data) => {
     const listData = data.results;
     const imageListEl = genImageListEl();
 
-    listData.map(photo => {
-        const imageCardEl = genImageCardEl(photo);
-        
-     imageListEl.append(imageCardEl);
+    listData.map((photo) => {
+      const imageCardEl = genImageCardEl(photo);
+
+      imageListEl.append(imageCardEl);
     });
 
     rootEl.append(imageListEl);
-});
-
-
-
-
-
-
-
-
+  });
