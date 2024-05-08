f86f990527d8674a0519db89814234aabfda0b66
Doriana <doriana.scaffo94@gmail.com>
2024-05-08 11:17:52 +0200

    Added exe-1-advanced.js lw_08 file to the correct folder

--- lw_08/exercises/js/exe-1-advanced.js
+++ lw_08/exercises/js/exe-1-advanced.js
@@ -1,35 +1,35 @@
-const createProduct = () => {
+const createProduct = (product) => {
   const containerEl = document.createElement("div");
   const titleEl = document.createElement("h1");
   const imageEl = document.createElement("img");
   const descrEl = document.createElement("p");
+  const priceEl = document.createElement("p");
 
   containerEl.className = "main--container";
   titleEl.className = "title";
-  titleEl.textContent = "My Store";
   imageEl.className = "image";
-  imageEl.src = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
-  imageEl.alt = "";
   descrEl.className = "description";
-  descrEl.textContent =
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve. Price: `$109.95`;";
+  priceEl.className = "price--product";
+  imageEl.src = product.image;
+  imageEl.alt = product.title;
+  titleEl.textContent = product.title;
+  descrEl.textContent = product.description;
+  priceEl.textContent = product.price;
 
-  containerEl.append(titleEl, imageEl, descrEl);
+  containerEl.append(titleEl, imageEl, descrEl, priceEl);
   return containerEl;
 };
 
 const rootEl = document.querySelector(".root");
 
-rootEl.append(createProduct());
-
 fetch("https://fakestoreapi.com/products/1")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    const product = createProduct(data);
+    
+        rootEl.append(product);
   })
   .catch((error) => {
     console.log(error);
   });
-//   .finally(() => {
-//     console.log("La chiamata è andata a buon fine");
-//   });
+
