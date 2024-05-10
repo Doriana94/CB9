1687efe5f1767a8377bdc8970b82b86ad0e682ec
Doriana <doriana.scaffo94@gmail.com>
2024-05-10 18:36:02 +0200

    Added file NavigationLinks.js

--- lw_09/07-05-24/js/components/NavigationLinks.js
+++ lw_09/07-05-24/js/components/NavigationLinks.js
@@ -1,16 +1,16 @@
-import { createEl } from '../helpers.js'
+import { createEl } from "../helpers.js";
 
 const NavigationLink = ({ href, text }) => {
-  const el = createEl('li')
-  const anchorEl = createEl('a')
+  const el = createEl("li");
+  const anchorEl = createEl("a");
 
-  el.className = 'navigation--link'
-  el.textContent = '> '
-  anchorEl.textContent = text
-  anchorEl.href = href
-  el.append(anchorEl)
+  el.className = "navigation--link";
+  el.textContent = "> ";
+  anchorEl.textContent = text;
+  anchorEl.href = href;
+  el.append(anchorEl);
 
-  return el
-}
+  return el;
+};
 
-export default NavigationLink
+export default NavigationLink;
