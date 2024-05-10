e554cdadf18b38da8570e7ea3d6287af3aa25c3b
Doriana <doriana.scaffo94@gmail.com>
2024-05-10 18:34:37 +0200

    Added file Image.js

--- lw_09/07-05-24/js/components/Image.js
+++ lw_09/07-05-24/js/components/Image.js
@@ -1,11 +1,11 @@
-import { createEl } from '../helpers.js'
+import { createEl } from "../helpers.js";
 
-const Image = ({ alt = 'This is an image', className = 'image', src } = {}) => {
-  const el = createEl('img')
-  el.alt = alt
-  el.src = src
-  el.className = className
-  return el
-}
+const Image = ({ alt = "This is an image", className = "image", src } = {}) => {
+  const el = createEl("img");
+  el.alt = alt;
+  el.src = src;
+  el.className = className;
+  return el;
+};
 
-export default Image
+export default Image;
