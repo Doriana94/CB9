5a63bda5445830561747622fa5a43abe489d2d28
Doriana <doriana.scaffo94@gmail.com>
2024-05-10 18:39:01 +0200

    Added file Text.js

--- lw_09/07-05-24/js/components/Text.js
+++ lw_09/07-05-24/js/components/Text.js
@@ -1,10 +1,10 @@
-import { createEl } from '../helpers.js'
+import { createEl } from "../helpers.js";
 
-const Text = ({ className = 'text', text }) => {
-  const el = createEl('p')
-  el.className = className
-  el.textContent = text
-  return el
-}
+const Text = ({ className = "text", text }) => {
+  const el = createEl("p");
+  el.className = className;
+  el.textContent = text;
+  return el;
+};
 
-export default Text
+export default Text;
