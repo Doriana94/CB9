8aa8d9d8c08ec530c937d6ed5b8721896bce6cc5
Doriana <doriana.scaffo94@gmail.com>
2024-05-10 18:32:07 +0200

    Added file App.js

--- lw_09/07-05-24/js/App.js
+++ lw_09/07-05-24/js/App.js
@@ -1,17 +1,17 @@
-import { createEl, find, log } from './helpers.js'
-import NavigationLink from './components/NavigationLink.js'
-import Product from './components/Product.js'
+import { createEl, find, log } from "./helpers.js";
+import NavigationLink from "./components/NavigationLink.js";
+import Product from "./components/Product.js";
 
-fetch('https://fakestoreapi.com/products/categories')
+fetch("https://fakestoreapi.com/products/categories")
   .then((res) => res.json())
   .then((categories) => {
     // Per ogni categoria, crea un componente di navigazione con link
     const navLinks = categories.map((cat, idx) =>
       NavigationLink({ href: `#${idx}`, text: cat })
-    )
-    const NAV_EL = find('.navigation')
-    NAV_EL.append(...navLinks)
-  })
+    );
+    const NAV_EL = find(".navigation");
+    NAV_EL.append(...navLinks);
+  });
 
 const createProductElement = (p) => {
   const productParams = {
@@ -22,18 +22,18 @@ const createProductElement = (p) => {
     name: p.title,
     // ****
     price: p.price,
-  }
-  const listEl = createEl('li')
-  const product = Product(productParams)
+  };
+  const listEl = createEl("li");
+  const product = Product(productParams);
 
-  listEl.append(product)
-  return listEl
-}
+  listEl.append(product);
+  return listEl;
+};
 
-fetch('https://fakestoreapi.com/products')
+fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((productsArray) => {
-    const productsList = productsArray.map(createProductElement)
-    const PRODUCTS_LIST_EL = find('.products--list')
-    PRODUCTS_LIST_EL.append(...productsList)
-  })
+    const productsList = productsArray.map(createProductElement);
+    const PRODUCTS_LIST_EL = find(".products--list");
+    PRODUCTS_LIST_EL.append(...productsList);
+  });
