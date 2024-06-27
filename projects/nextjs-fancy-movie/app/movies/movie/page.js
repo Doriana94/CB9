6aff4ce523236ab764b3c1d8bad52124d068c61a
Doriana <doriana.scaffo94@gmail.com>
2024-06-27 18:14:59 +0200

    first commit

---
+++ projects/nextjs-fancy-movie/app/movies/movie/page.js
@@ -0,0 +1,8 @@
+
+const Movie = () => {
+    return(
+        <h1>Ciao, io sono movie</h1>
+    )
+}
+
+export default Movie;
---
+++ projects/nextjs-fancy-movie/app/movies/page.js
@@ -0,0 +1,11 @@
+
+const Movies = () => {
+    return(
+        <main>
+            <h1>Ciao io sono movies</h1>
+           {/* <Card /> */}
+        </main>
+    )
+}
+
+export default Movies;
